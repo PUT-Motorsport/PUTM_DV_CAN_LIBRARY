34c6543530f90764e25e16bf974e0f353b0dd996
KozAAAAA <mateusz-koza@outlook.com>
2023-03-03 14:14:19 +0100

    Add complete and tested can interface

--- main.cpp
+++ main.cpp
@@ -3,22 +3,23 @@
 
 using namespace PUTM_CAN;
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     CAN can;
     can.connect();
 
     Apps_main apps_frame;
 
     apps_frame = {
-        .pedal_position = 0,
-        .counter = 1,
-        .position_diff = 2,
-        .device_state = Apps_states::Power_up
+        .pedal_position = 1,
+        .counter = 2,
+        .position_diff = 3,
+        .device_state = Apps_states::Sensor_Implausiblity
     };
-
+    
     can.transmit(apps_frame);
     can.receive(apps_frame);
     can.receive_rtr(apps_frame);
 
     return 0;
-}
+}SS
