db9ba6e97099d6f225e6bf2a8009bcecd5cd7c28
KozAAAAA <mateusz-koza@outlook.com>
2023-03-07 15:53:04 +0100

    Add header only library

--- Inc/putm_can_interface.hpp
+++ Inc/putm_can_interface.hpp
@@ -45,7 +45,6 @@ namespace PUTM_CAN
 
 }
 
-
 namespace PUTM_CAN
 {
     template <typename T>
@@ -109,6 +108,104 @@ namespace PUTM_CAN
         }
         return 0;
     }
+
+    int8_t CAN::connect(const char *ifname)
+    {
+        struct ifreq ifr;
+        struct sockaddr_can addr;
+        if ((private_socket = socket(PF_CAN, SOCK_RAW, CAN_RAW)) == -1)
+        {
+            return -1;
+        }
+        strcpy(ifr.ifr_name, ifname);
+        ioctl(private_socket, SIOCGIFINDEX, &ifr);
+        addr.can_family = AF_CAN;
+        addr.can_ifindex = ifr.ifr_ifindex;
+        if (bind(private_socket, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+        {
+            return -2;
+        }
+
+        return 0;
+    }
+
+    int8_t CAN::disconnect()
+    {
+        if (close(private_socket) < 0)
+        {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    int8_t CAN::bytes_transmit(const uint16_t &can_id, const uint8_t &can_dlc, const char *tx_data)
+    {
+        struct can_frame frame;
+        frame.can_id = can_id;
+        frame.can_dlc = can_dlc;
+
+        std::memcpy(frame.data, tx_data, can_dlc);
+
+        if (write(private_socket, &frame, sizeof(struct can_frame)) < 0)
+        {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    int8_t CAN::bytes_receive(const uint16_t &can_id, const uint8_t &can_dlc, char *rx_data)
+    {
+        struct can_frame frame;
+        struct can_filter filter
+        {
+            .can_id = can_id,
+            .can_mask = CAN_SFF_MASK
+        };
+
+        if (setsockopt(private_socket, SOL_CAN_RAW, CAN_RAW_FILTER, &filter, sizeof(filter)) != 0)
+        {
+            return -1;
+        }
+
+        if (read(private_socket, &frame, sizeof(struct can_frame)) < sizeof(can_frame))
+        {
+            return -2;
+        }
+
+        std::memcpy(rx_data, frame.data, can_dlc);
+
+        return 0;
+    }
+
+    int8_t CAN::bytes_receive_rtr(const uint16_t &can_id, const uint8_t &can_dlc, char *rx_data)
+    {
+        struct can_frame frame;
+        frame.can_id = can_id | CAN_RTR_FLAG;
+
+        if (write(private_socket, &frame, sizeof(struct can_frame)) < 0)
+        {
+            return -1;
+        }
+
+        if (bytes_receive(can_id, can_dlc, rx_data) != 0)
+        {
+            return -2;
+        }
+
+        return 0;
+    }
+
+    int8_t CAN::structure_receive_random(can_frame &rx_frame)
+    {
+        if(read(private_socket, &rx_frame, sizeof(can_frame)) < sizeof(can_frame))
+        {
+            return -1;
+        }
+        return 0;
+    }
+
 }
 
 #endif // PUTM_CAN_INTERFACE
