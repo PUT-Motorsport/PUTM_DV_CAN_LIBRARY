3b2cf69856937f79897322fb928c0288a2f6c04e
KozAAAAA <mateusz-koza@outlook.com>
2023-03-12 19:47:59 +0100

    Add timeout functionality

--- Inc/putm_can_interface.hpp
+++ Inc/putm_can_interface.hpp
@@ -16,16 +16,17 @@
 
 namespace PUTM_CAN
 {
-    
+    constexpr time_t NO_TIMEOUT = 0;
+
     enum class CanState : uint8_t
     {
         CAN_OK,
         CAN_SOCKET_ERROR,
         CAN_BIND_ERROR,
-        CAN_MASKING_ERROR,
+        CAN_SET_MASK_ERROR,
+        CAN_SET_TIMEOUT_ERROR,
         CAN_WRITE_ERROR,
         CAN_READ_ERROR,
-        CAN_RECEIVE_ERROR,
         CAN_CLOSE_ERROR
     };
 
@@ -36,27 +37,25 @@ namespace PUTM_CAN
 
     public:
         inline CAN() = default;
+
         inline CanState connect(const char *ifname = "slcan0");
         inline CanState disconnect();
+
+        inline CanState structure_receive_random(can_frame &rx_frame, time_t timeout_in_seconds);
+
+        template <typename T>
+        inline CanState transmit(T const &tx_frame);
+        template <typename T>
+        inline CanState receive(T &rx_frame, time_t timeout_in_seconds);
+        template <typename T>
+        inline CanState receive_rtr(T &rx_frame, time_t timeout_in_seconds);
 
         inline CanState bytes_transmit(const uint16_t &can_id, const uint8_t &can_dlc, const char *tx_data);
         inline CanState bytes_receive(const uint16_t &can_id, const uint8_t &can_dlc, char *rx_data);
         inline CanState bytes_receive_rtr(const uint16_t &can_id, const uint8_t &can_dlc, char *rx_data);
-
-        inline CanState structure_receive_random(can_frame &rx_frame);
-
-        template <typename T>
-        inline CanState transmit(T const &tx_frame);
-
-        template <typename T>
-        inline CanState receive(T &rx_frame);
-
-        template <typename T>
-        inline CanState receive_rtr(T &rx_frame);
     };
 
 }
-
 
 namespace PUTM_CAN
 {
@@ -64,12 +63,12 @@ namespace PUTM_CAN
     inline CanState CAN::transmit(T const &tx_frame)
     {
         static_assert(can_id<T> != INVALID_CAN_ID, "Can id has not been specified");
-        
-        struct can_frame frame;
+
+        can_frame frame;
         frame.can_id = can_id<T>;
         frame.can_dlc = sizeof(T);
         std::memcpy(frame.data, &tx_frame, sizeof(T));
-        if (write(private_socket, &frame, sizeof(struct can_frame)) < 0)
+        if (write(private_socket, &frame, sizeof(can_frame)) < 0)
         {
             return CanState::CAN_WRITE_ERROR;
         }
@@ -77,51 +76,53 @@ namespace PUTM_CAN
     }
 
     template <typename T>
-    inline CanState CAN::receive(T &rx_frame)
+    inline CanState CAN::receive(T &rx_frame, time_t timeout_in_seconds)
     {
         static_assert(can_id<T> != INVALID_CAN_ID, "Can id has not been specified");
-        
-        struct can_frame frame;
-        struct can_filter filter
+
+        can_frame frame;
+        can_filter filter = {can_id<T>, CAN_SFF_MASK};
+
+        timeval tv = {timeout_in_seconds, 0};
+        if (setsockopt(private_socket, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof tv) != 0)
         {
-            can_id<T>,
-            CAN_SFF_MASK
-        };
+            return CanState::CAN_SET_TIMEOUT_ERROR;
+        }
+
         if (setsockopt(private_socket, SOL_CAN_RAW, CAN_RAW_FILTER, &filter, sizeof(filter)) != 0)
         {
-            return CanState::CAN_MASKING_ERROR;
+            return CanState::CAN_SET_MASK_ERROR;
         }
-        if (read(private_socket, &frame, sizeof(struct can_frame)) < sizeof(can_frame))
+
+        if (read(private_socket, &frame, sizeof(can_frame)) < (ssize_t)sizeof(can_frame))
         {
             return CanState::CAN_READ_ERROR;
         }
         std::memcpy(&rx_frame, frame.data, sizeof(T));
-        
+
         return CanState::CAN_OK;
     }
 
     template <typename T>
-    inline CanState CAN::receive_rtr(T &rx_frame)
+    inline CanState CAN::receive_rtr(T &rx_frame, time_t timeout_in_seconds)
     {
         static_assert(can_id<T> != INVALID_CAN_ID, "Can id has not been specified");
-        
-        struct can_frame frame;
+
+        can_frame frame;
         frame.can_id = can_id<T> | CAN_RTR_FLAG;
-        if(write(private_socket, &frame, sizeof(struct can_frame)) < 0)
+
+        if (write(private_socket, &frame, sizeof(can_frame)) < 0)
         {
             return CanState::CAN_WRITE_ERROR;
         }
-        if(receive(rx_frame)!=CanState::CAN_RECEIVE_ERROR)
-        {
-            return CanState::CAN_RECEIVE_ERROR;
-        }
-        return CanState::CAN_OK;
+
+        return receive(rx_frame, timeout_in_seconds);
     }
 
     inline CanState CAN::connect(const char *ifname)
     {
-        struct ifreq ifr;
-        struct sockaddr_can addr;
+        ifreq ifr;
+        sockaddr_can addr;
         if ((private_socket = socket(PF_CAN, SOCK_RAW, CAN_RAW)) == -1)
         {
             return CanState::CAN_SOCKET_ERROR;
@@ -130,7 +131,7 @@ namespace PUTM_CAN
         ioctl(private_socket, SIOCGIFINDEX, &ifr);
         addr.can_family = AF_CAN;
         addr.can_ifindex = ifr.ifr_ifindex;
-        if (bind(private_socket, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+        if (bind(private_socket, (sockaddr *)&addr, sizeof(addr)) == -1)
         {
             return CanState::CAN_BIND_ERROR;
         }
@@ -148,15 +149,30 @@ namespace PUTM_CAN
         return CanState::CAN_OK;
     }
 
+    inline CanState CAN::structure_receive_random(can_frame &rx_frame, time_t timeout_in_seconds)
+    {
+        timeval tv = {timeout_in_seconds, 0};
+        if (setsockopt(private_socket, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof tv) != 0)
+        {
+            return CanState::CAN_SET_TIMEOUT_ERROR;
+        }
+
+        if (read(private_socket, &rx_frame, sizeof(can_frame)) < (ssize_t)sizeof(can_frame))
+        {
+            return CanState::CAN_READ_ERROR;
+        }
+        return CanState::CAN_OK;
+    }
+
     inline CanState CAN::bytes_transmit(const uint16_t &can_id, const uint8_t &can_dlc, const char *tx_data)
     {
-        struct can_frame frame;
+        can_frame frame;
         frame.can_id = can_id;
         frame.can_dlc = can_dlc;
 
         std::memcpy(frame.data, tx_data, can_dlc);
 
-        if (write(private_socket, &frame, sizeof(struct can_frame)) < 0)
+        if (write(private_socket, &frame, sizeof(can_frame)) < 0)
         {
             return CanState::CAN_WRITE_ERROR;
         }
@@ -166,19 +182,15 @@ namespace PUTM_CAN
 
     inline CanState CAN::bytes_receive(const uint16_t &can_id, const uint8_t &can_dlc, char *rx_data)
     {
-        struct can_frame frame;
-        struct can_filter filter
-        {
-            can_id,
-            CAN_SFF_MASK
-        };
+        can_frame frame;
+        can_filter filter = {can_id, CAN_SFF_MASK};
 
         if (setsockopt(private_socket, SOL_CAN_RAW, CAN_RAW_FILTER, &filter, sizeof(filter)) != 0)
         {
             return CanState::CAN_SOCKET_ERROR;
         }
 
-        if (read(private_socket, &frame, sizeof(struct can_frame)) < sizeof(can_frame))
+        if (read(private_socket, &frame, sizeof(can_frame)) < (ssize_t)sizeof(can_frame))
         {
             return CanState::CAN_READ_ERROR;
         }
@@ -190,29 +202,14 @@ namespace PUTM_CAN
 
     inline CanState CAN::bytes_receive_rtr(const uint16_t &can_id, const uint8_t &can_dlc, char *rx_data)
     {
-        struct can_frame frame;
+        can_frame frame;
         frame.can_id = can_id | CAN_RTR_FLAG;
 
-        if (write(private_socket, &frame, sizeof(struct can_frame)) < 0)
+        if (write(private_socket, &frame, sizeof(can_frame)) < 0)
         {
             return CanState::CAN_WRITE_ERROR;
         }
 
-        if (bytes_receive(can_id, can_dlc, rx_data) != CanState::CAN_OK)
-        {
-            return CanState::CAN_RECEIVE_ERROR;
-        }
-
-        return CanState::CAN_OK;
+        return bytes_receive(can_id, can_dlc, rx_data);
     }
-
-    inline CanState CAN::structure_receive_random(can_frame &rx_frame)
-    {
-        if(read(private_socket, &rx_frame, sizeof(can_frame)) < sizeof(can_frame))
-        {
-            return CanState::CAN_READ_ERROR;
-        }
-        return CanState::CAN_OK;
-    }
-
 }
